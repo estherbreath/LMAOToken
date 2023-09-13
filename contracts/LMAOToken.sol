a338e037f4b13fb98d74d212a9f0da3707560583
Esther Breath <estherojile248@gmail.com>
2023-09-13 01:29:50 +0000

    first commit

---
+++ contracts/LMAOToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract LMAOToken is ERC20("LMAOToken", "LTK"){
+      address owner;
+      constructor() {
+            owner = msg.sender;
+      _mint(owner, 1000e18);
+      }
+function _transfer(address from, address to, uint256 amount) internal virtual override {
+      uint feeOnTransfer = (8 * amount) / 100;
+      uint remainder = amount - feeOnTransfer;
+      super._transfer(from, to, remainder);
+      super._transfer(from, owner, feeOnTransfer);
+}
+
+
+
+}
---
+++ contracts/WLMAOToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+
+contract WLMAOToken is ERC20("WLMAOToken", "WTK"){
+    IERC20 LMAO;
+      constructor(address _LMAO) {
+            LMAO = IERC20(_LMAO);
+      }
+
+      function deposit(uint _amount) public {
+        LMAO.transferFrom(msg.sender, address(this), _amount);
+        uint calcAmount = (92 * _amount) / 100;
+        _mint(msg.sender, calcAmount);
+      }
+
+      function swapToken (uint _amount) public {
+        require (balanceOf (msg.sender) >= _amount, "balance must be greater than amount");
+        LMAO.transfer(msg.sender, _amount);
+        _burn(msg.sender, _amount);
+
+
+      }
+
+
+}
